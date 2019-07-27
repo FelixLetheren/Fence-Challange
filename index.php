28fa624067dcd9c27d0215f99158b12c77d86bf6
Felix Letheren <felix.letheren@gmail.com>
2019-07-27 20:17:33 +0100

    Completed first feature Can calculate length of fence from given railings and posts

--- index.php
+++ index.php
@@ -1,16 +1,43 @@
 <!DOCTYPE html>
 <?php
-define('RAILING_LENGTH', 1.5 );
+define('RAILING_LENGTH', 1.5);
 define('POST_LENGTH', 0.1);
 $userInputPosts = $_GET['userInputPosts'];
 $userInputRailings = $_GET['userInputRailings'];
 
-function addRailings($railings){
-
+/**
+ * @param int $userInputRailings
+ * @param int $userInputPosts
+ * @return float|int
+ * Takes posts and railings input by user and determines the amount of posts
+ * and fences that can be used to build the fence. Then calls addPostAndRailingLength
+ */
+function getMaxLength(int $userInputRailings, int $userInputPosts)
+{
+    if ($userInputPosts > $userInputRailings) {
+        $posts = $userInputRailings + 1;
+        $railings = $userInputRailings;
+    } else {
+        $railings = $userInputPosts - 1;
+        $posts = $userInputPosts;
+    }
+    return addPostandRailingLength($posts, $railings);
 }
 
-//echo 'you have input ' .$_GET['userInputPosts']. ' posts and ' .$_GET['userInputRailings']. ' Railings.'
-echo 'You have input ' .$userInputPosts. ' posts and ' .$userInputRailings. ' railings.'
+/**
+ * @param int $posts
+ * @param int $railings
+ * @return float|int
+ * Takes posts and railings and multiplies them by their length and adds together.
+ */
+function addPostandRailingLength(int $posts, int $railings){
+    $postLength = $posts * POST_LENGTH;
+    $railingLength = $railings * RAILING_LENGTH;
+    $totalLength = $postLength + $railingLength;
+    return $totalLength;
+}
+echo 'You have input ' . $userInputPosts . ' posts and ' . $userInputRailings . ' railings.<br>';
+echo 'The maximum length of fence that can be created is '. getMaxLength($userInputRailings, $userInputPosts);
 ?>
 <html lang="en">
 <meta charset="UTF-8">
---
+++ test/IndexTest.php
@@ -0,0 +1,30 @@
+<?php
+require '../index.php';
+
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testgetMaxLength_givesCorrectLength()
+    {
+
+        $expectedResult = 27.3;
+
+        $result = getMaxLength(17, 30);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testPostAndRailingLength_calculatesCorrectly()
+    {
+        $expectedResult = 8.1;
+
+        $result = addPostandRailingLength(6, 5);
+
+        $this->assertEquals($expectedResult, $result);
+
+    }
+
+
+
+}
